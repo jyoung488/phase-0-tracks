42179de1f5f678e19945c8b73c4f59b6d5c4981f
jyoung488 <jyoung488@gmail.com>
2016-10-03 15:53:04 -0700

    Add conditions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -12,3 +12,15 @@ garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance? Yes/No"
 insurance = gets.chomp
+
+current_year = 2016
+
+if (age = current_year - year.to_i) && (garlic_bread == "Yes") && (insurance == "Yes")
+  puts "Probably not a vampire."
+
+elsif (insurance == "No") || [(age <=> current_year - year.to_i) && (garlic_bread == "No")]
+  puts "Probably a vampire."
+
+elsif (insurance == "No") && (age <=> current_year - year.to_i) && (garlic_bread == "No")
+  puts "Almost certainly a vampire."
+end  
