e2f1c628d74739ca0d563b673d409358b8f6d28b
jyoung488 <jyoung488@gmail.com>
2016-10-03 15:08:34 -0700

    Add additional questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,2 +1,14 @@
 puts "What is your name?"
 name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? Yes/No"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? Yes/No"
+insurance = gets.chomp
