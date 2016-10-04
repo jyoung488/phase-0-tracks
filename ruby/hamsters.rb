9038124c6da56404115e6c01dc1955170588bd2e
jyoung488 <jyoung488@gmail.com>
2016-10-04 15:40:40 -0700

    Add responses

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,7 @@ puts "What is the hamster's name?"
 hamster_name = gets.chomp
 
 puts "What's the volume level from 1-10 of the hamster?"
-hamster_volume = gets.to_i
+hamster_volume = gets.to_f
 
 puts "What's the fur color?"
 hamster_color = gets.chomp
@@ -11,10 +11,19 @@ puts "Is the hamster a good candidate for adoption? Yes/No"
 adoption_candidacy = gets.chomp
 
 puts "What's the hamster's estimated age?"
-estimated_age = gets.to_i
+estimated_age = gets.chomp
 
-if estimated_age == ""
-  estimated_age = nil
-else estimated_age == true
+if estimated_age.empty? 
+  puts "No estimated age"
+else
+    puts "Your hamster's estimated age is #{estimated_age}"
 end
 
+puts "Your hamster's name is #{hamster_name} and its fur color is #{hamster_color}. 
+Its volume level is #{hamster_volume}."
+
+if adoption_candidacy == "Yes"
+  puts "It is a good candidate"
+else
+  puts "Not a good candidate"
+end
