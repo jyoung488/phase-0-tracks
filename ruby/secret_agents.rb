a067a2884ca1788c2ede6b61f7f203d63c5ca6ae
jyoung488 <jyoung488@gmail.com>
2016-10-06 20:58:32 -0700

    Add encrypt method pseudocode

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,8 +1,16 @@
+=begin
+  
+Define a method called encrypt that takes user input for a string
+returns a string with each letter of the initial string moved forward
+one letter in the alphabet, keeping spaces as they are.
+  
+=end
+
 def encrypt
   
   puts "Give me a string"
   string = gets.chomp
-  
+
 index = 0
   while index < string.length
     unless string[index] == " "
