115741f373f884e4e5dc46fc281f050abd4fcf69
jyoung488 <jyoung488@gmail.com>
2016-10-06 19:13:52 -0700

    Create encrypt method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+def encrypt
+  
+  puts "Give me a string"
+  string = gets.chomp
+  
+index = 0
+  while index < string.length
+    string[index] = string[index].next
+    index += 1
+  end
+  
+  puts string
+end
