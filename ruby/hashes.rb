d00d7a310c25b10759a8a2164ac4f23d3dc1fd90
jyoung488 <jyoung488@gmail.com>
2016-10-10 14:56:34 -0700

    Add user input questions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,3 +6,14 @@
 # number of rooms in house
 # do you want bathrooms decorated?
 # favorite color
+
+
+client_details = {}
+
+puts "What is your name?"
+puts "How old are you?"
+puts "How many children do you have?"
+puts "What's your preferred decor theme?"
+puts "How many rooms do you have in your house?"
+puts "Do you want your bathrooms decorated?"
+puts "What's your favorite color?"
