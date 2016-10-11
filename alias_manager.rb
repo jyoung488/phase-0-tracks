9151464fa56f970a29a9eaf1fc977dcb361114d5
jyoung488 <jyoung488@gmail.com>
2016-10-11 09:54:45 -0700

    Define method to switch first and last names

--- alias_manager.rb
+++ alias_manager.rb
@@ -3,3 +3,16 @@
 # change vowels to next vowel in 'aeiou'
 # change consonants to next consonant
 
+puts "What is your real name?"
+full_name = gets.chomp
+
+def name_switch(full_name)
+
+name_array = full_name.split
+
+new_first_name = name_array[1]
+new_last_name = name_array[0]
+
+switched_name = "#{new_first_name} " + "#{new_last_name}"
+switched_name
+end
