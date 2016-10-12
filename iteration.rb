87f77a23d82a60dc00f2ed2713828da20fdede40
jyoung488 <jyoung488@gmail.com>
2016-10-12 14:23:29 -0700

    Add iteration through array and hash

--- iteration.rb
+++ iteration.rb
@@ -17,9 +17,33 @@ end
 p cookies
 
 bakery_items = { 
-    pastry: "croissant",
+    pastries: "croissant",
     breads: "bagels",
     desserts: "cupcake"
 }
 
-bakery_items.each {|item,type| puts "I like #{item} and more specifically #{type}!"}
+bakery_items.each {|type, item| puts "I like #{type} and more specifically #{item}!"}
+
+numbers = [1, 2, 4, 5, 6]
+numbers.delete_if {|number| number < 4 }
+
+numbers.keep_if {|number| number < 4}
+
+numbers.reject! { |number| number < 4 }
+
+numbers.select! { |number| number.odd? }
+
+state_capitols = { 
+    California: "Sacramento",
+    New_York: "Albany",
+    Texas: "Austin",
+    Arizona: "Phoenix"
+}
+
+state_capitols.delete_if { |state, city| state.length < 6}
+
+state_capitols.keep_if { |state, city| state.length < 6}
+
+state_capitols.reject! { |state, city| city.length == 6}
+
+state_capitols.select! { |state, city| city.include? "A" }
