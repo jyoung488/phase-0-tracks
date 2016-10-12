1094c2e66c95b6dd8b8e7ade0dd28482fed2b95a
jyoung488 <jyoung488@gmail.com>
2016-10-12 10:57:48 -0700

    Add vowel conditional statement

--- alias_manager.rb
+++ alias_manager.rb
@@ -16,3 +16,20 @@ new_last_name = name_array[0]
 switched_name = "#{new_first_name} " + "#{new_last_name}"
 switched_name
 end
+
+vowels = "aeiou".split("")
+consonants = "bcdfghjklmnpqrstvwxyz".split("")
+
+new_name = []
+index = 0
+
+name_array.each do | letter |
+  if vowels.include? letter
+    x = vowels [index + 1]
+    new_name << x
+  else
+    nil
+  end
+end
+
+puts new_name.join("")
