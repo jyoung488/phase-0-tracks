314002088f8a1ed40e856a26d3a56f0071c128aa
jyoung488 <jyoung488@gmail.com>
2016-10-12 14:24:17 -0700

    Add consonant conditional

--- alias_manager.rb
+++ alias_manager.rb
@@ -3,9 +3,6 @@
 # change vowels to next vowel in 'aeiou'
 # change consonants to next consonant
 
-puts "What is your real name?"
-full_name = gets.chomp
-
 def name_switch(full_name)
 
 name_array = full_name.split
@@ -14,22 +11,30 @@ new_first_name = name_array[1]
 new_last_name = name_array[0]
 
 switched_name = "#{new_first_name} " + "#{new_last_name}"
-switched_name
+switched_name_array = switched_name
 end
 
+def next_vowel(name_switch)
+switched_name_array = name_switch.split("")
 vowels = "aeiou".split("")
 consonants = "bcdfghjklmnpqrstvwxyz".split("")
 
 new_name = []
 index = 0
 
-name_array.each do | letter |
+switched_name_array.each do | letter |
   if vowels.include? letter
     x = vowels [index + 1]
+    new_name << x
+  elsif consonants.include? letter
+    x = consonants[index + 1]
     new_name << x
   else
     nil
   end
 end
+end
 
-puts new_name.join("")
+puts "What is your real name?"
+full_name = gets.chomp
+puts next_vowel(name_switch(full_name))
