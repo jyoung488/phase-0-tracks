9261c8fd64b2e755b8300190126f39c062eb17ec
jyoung488 <jyoung488@gmail.com>
2016-10-13 11:31:53 -0700

    Add Fibonacci method

--- problem_solving_methods.rb
+++ problem_solving_methods.rb
@@ -12,3 +12,21 @@ end
 
 arr = [1, 2, 3, 4, 5]
 p search_array(arr, 3)
+
+def fibs(num)
+ x = 0
+ y = 1
+ arr = []
+
+ num.times { 
+   z = x + y 
+   arr << x
+   x = y
+   y = z
+ }
+ arr
+end
+
+fibs(100)
+
+fibs(100)[-1] == 218922995834555169026
