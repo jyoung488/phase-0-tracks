a2b03a3613d9f06553d2b09eb65e00ddb35e0a50
jyoung488 <jyoung488@gmail.com>
2016-10-17 09:27:22 -0700

    Add puppy characteristics in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+  
+SPECIES ---
+Puppius Extraordinaire
+
+CHARACTERISTICS ---
+Name: varies
+Leg count: 4
+Friendly: yes
+Color: varies
+Tail length: varies
+
+BEHAVIOR ---
+Lick
+Sniff
+Fetch
+Cuddle
+  
+=end
