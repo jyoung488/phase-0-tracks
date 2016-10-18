16999a27bf68b0622555bfc237d7470bb394eeba
jyoung488 <jyoung488@gmail.com>
2016-10-18 10:07:20 -0700

    Define mixin version of Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,3 +11,14 @@ end
 
 puts Shout.yell_angrily("Hey")
 puts Shout.yelling_happily("I can't wait")
+=end
+
+module Shout
+  def angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def happily(words)
+    words + " yay!!!"
+  end
+end
