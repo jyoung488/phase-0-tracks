7c949b6c3f1574ceb23fe7041067e81fc60d8318
jyoung488 <jyoung488@gmail.com>
2016-10-18 16:25:40 -0700

    Add reflection comments

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -36,3 +36,34 @@ change_quantity(todays_list, "lemonade", 2)
 change_quantity(todays_list, "tomatoes", 3)
 delete_item(todays_list, "lemonade")
 print_list(todays_list)
+
+=begin
+  Coming up with detailed pseudocode for the entire problem helped so much. It allowed me and my pair to
+  split up the problem into smaller chunks and we could test small parts of our code and know exactly
+  what the issue was. I definitely found the use of pseudocode with this assignment and will use it for
+  all of it before diving straight into the code. I also now understand that pseudocode doesn't need to
+  stay in the assignment as long as the code is readable.
+
+  If we had used an array, we couldn't set the quantity amounts for each item, it would have to have been
+  separate arrays for each item and quantity. Using a hash allowed us to store it all at once but we had to
+  access the information inside the hash structure a different way and make sure we used the correct keys
+  and values.
+
+  A method returns the value you set to return at the end of it. For instance, we set the method to return a
+  hash that we'd given a variable name of list. It can also make changes to a parameter given without
+  returning a specific value.
+
+  A method can take strings, integers, hashes, arrays as arguments.
+
+  Information can be passed between methods using a class but in this case, setting the method to equal a
+    variable that's then called in the driver code of another method.
+
+    Going through and refactoring the code with the guide's help showed me how to examine code that SEEMS
+    readable already but could still use improvement. I feel much more comfortable with defining methods
+    that accept arguments and making them available to other methods. I'm still feeling a bit rusty with
+    nesting methods in one another, for example if we could call print_list(create_list("apples oranges
+     potatoes"))? I believe it would but I will be practicing with more complex problems!
+
+  
+=end
+
