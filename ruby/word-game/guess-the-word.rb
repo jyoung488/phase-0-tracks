c285dd363cf41a4bda36b9840f1a76a7f2a2e28b
jyoung488 <jyoung488@gmail.com>
2016-10-19 15:52:16 -0700

    Add pseudocode

---
+++ ruby/word-game/guess-the-word.rb
@@ -0,0 +1,19 @@
+  # game class
+
+  # initialize method
+    # expects argument of a word
+    # outputs blank lines _ for each length of the word
+
+  # match method
+    # if letter input is in the initial word, add it to output
+    # if not, output same
+
+  # maximum guesses is the length of the word
+    # count doesn't increase if letter already guessed
+
+  # user interface
+    # greets the user and asks for an input
+    # initializes new instance of game with input as first word
+
+    # asks for letter guesses input
+      # iterate over word's letters to see if input equals any
