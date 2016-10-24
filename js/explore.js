451699013b88a3930b2f2380db768a1df7c6f689
jyoung488 <jyoung488@gmail.com>
2016-10-24 12:08:15 -0700

    Fix reverse string function return value and add conditional statements

--- js/explore.js
+++ js/explore.js
@@ -8,9 +8,16 @@ function reverseString(string) {
   for (var index = string.length - 1; index >= 0; index--) {
     newString += string[index];
   }
-  console.log(newString);
+  return newString;
 }
 
-  reverseString("hello");
+reverseString("hello");
 
-  var backwardsWord = reverseString("computer");
+var backwardsWord = reverseString("computer");
+
+  if (1==1 && 2>0) {
+    console.log(backwardsWord);
+  }
+  else {
+    console.log("not equal!")
+  }
