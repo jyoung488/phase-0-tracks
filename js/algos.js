9b91486fc9d3162708a2cbe7a00edce832fab55d
jyoung488 <jyoung488@gmail.com>
2016-10-25 14:47:38 -0700

    Define function that takes array as a parameter

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// define a function longestPhrase
+// takes input of an array of phrases
+// call .length method on each phrase listed
+// output longest phrase
+
+function longestPhrase(array) {
+
+}
+
+longestPhrase(["hello there", "this is the longest phrase", "hi"]);
