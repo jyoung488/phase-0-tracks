3045689efaa5cc120f4fcddd88b939b35b7208cb
jyoung488 <jyoung488@gmail.com>
2016-10-25 15:23:03 -0700

    Define function that returns longest phrase in array and driver code

--- js/algos.js
+++ js/algos.js
@@ -1,10 +1,18 @@
 // define a function longestPhrase
 // takes input of an array of phrases
 // call .length method on each phrase listed
-// output longest phrase
+// list phrases from longest to shortest
+// output longest phrase using index 0
 
 function longestPhrase(array) {
+  var phrase = ""
 
-}
+  for (var index = 0; index < array.length; index++) {
+    if (array[index].length > phrase.length) {
+      phrase = array[index];
+    }
+  }
+    console.log(phrase);
+  }
 
 longestPhrase(["hello there", "this is the longest phrase", "hi"]);
