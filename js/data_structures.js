054e73d5f39022e2db369a397134a87452b5e84a
jyoung488 <jyoung488@gmail.com>
2016-10-26 13:19:59 -0700

    Assign arrays to key value pairs

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,13 @@
 var colors = ["blue", "green", "brown", "black"];
-var horseNames = ["Black Beauty", "Ed", "Gus", "Randolph"]
+var horseNames = ["Black Beauty", "Ed", "Gus", "Randolph"];
 
 colors.push("purple");
 horseNames.push("Cinnamon");
+
+var horses = {}
+
+if (colors.length = horseNames.length) {
+  for (var index = 0; index < horseNames.length; index++) {
+    horses[horseNames[index]] = colors[index];
+  }
+}
