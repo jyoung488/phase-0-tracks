ec12927e9b5646b31c9bd4b24b58354b55f2e014
jyoung488 <jyoung488@gmail.com>
2016-10-25 23:44:58 -0700

    Refactor and remove extra variable definition

--- js/algos.js
+++ js/algos.js
@@ -1,19 +1,17 @@
-// define a function longestPhrase
-// takes input of an array of phrases
-// call .length method on each phrase listed
-// list phrases from longest to shortest
-// output longest phrase using index 0
+  // define a function longestPhrase that takes input
+  // call .length method on each element in array
+  // output longest phrase using index 0
 
-function longestPhrase(array) {
-  var phrase = ""
+  function longestPhrase(array) {
+    var phrase = ""
 
-  for (var index = 0; index < array.length; index++) {
-    if (array[index].length > phrase.length) {
-      phrase = array[index];
+    for (var index = 0; index < array.length; index++) {
+      if (array[index].length > phrase.length) {
+        phrase = array[index];
+      }
     }
-  }
     return phrase;
-  }
+    }
 
   // define function taking 2 parameters of key-value pairs
   // compare the keys of each pair
@@ -30,61 +28,56 @@ function longestPhrase(array) {
 
     for (var key2 in pair2) {
       pair2Keys.push(key2);
+      }
+
+      if (pair1Keys[0] == pair2Keys[0]) {
+        console.log("true");
+      } else if (pair1Keys[1] == pair2Keys[1]) {
+        console.log("true");
+      } else {
+        console.log("false");
+      }
     }
 
-    if (pair1Keys[0] == pair2Keys[0]) {
-      console.log("true"); }
-    else if (pair1Keys[1] == pair2Keys[1]) {
-      console.log("true");
-    }
-    else {console.log("false");}
+
+  // define function that takes a parameter of string length
+  // set parameter to create an array of phrases with that integer as the length
+  // so array.length = integer
+  // each word in the array should be a random length between 1-10 letters
+
+  function randomString(length) {
+      var randomText = "";
+      var possible = "abcdefghijklmnopqrstuvwxyz";
+      for(var i = 0; i < length; i++) {
+          randomText += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return randomText;
   }
 
+  function randomPhrases(num) {
+    var phrases = [];
 
-// define function that takes an integer as a parameter
-// set that parameter to create an array of phrases with that integer as the length
-// so array.length = integer
-// each word in the array should be a random length between 1-10 letters
-
-function randomString(length) {
-    var text = "";
-    var possible = "abcdefghijklmnopqrstuvwxyz";
-    for(var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for (var length = 0; length < num; length++) {
+      phrases.push(randomString(Math.floor(Math.random() * 10) +1));
     }
-    return text;
-}
-
-function randomPhrases(num) {
-  var phrases = [];
-  var phrasesLength = 0;
-
-  for (phrasesLength = 0; phrasesLength < num; phrasesLength++) {
-    phrases.push(randomString(Math.floor(Math.random() * 10) +1));
+    return phrases;
   }
-  return phrases;
-}
 
+  // test driver code
 
-longestPhrase(["hello there", "this is the longest phrase", "hi"]);
-matchKeys({name: "Steven", age: 54}, {name: "Tamir", age: 54});
-matchKeys({color: "orange", age: 20}, {hobby: "sleeping", food: "apples"});
+  console.log(longestPhrase(["hello there", "this is the longest phrase", "hi"]));
+  matchKeys({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+  matchKeys({color: "orange", age: 20}, {hobby: "sleeping", food: "apples"});
 
-// loop 10 times
-// generates array
-// prints array
-// feed array into longestPhrase function
-// prints result of longest phrase
+  // loop 10 times
+  // generates then prints array
+  // feed array into longestPhrase function and print result
 
-for (i = 0; i < 10; i++) {
-  var finalArray = randomPhrases(4);
-  console.log(finalArray);
-  var finalPhrase = longestPhrase(finalArray);
-  console.log(finalPhrase);
-}
-
-
-
-
+  for (i = 0; i < 10; i++) {
+    var finalArray = randomPhrases(4);
+    console.log(finalArray);
+    var finalPhrase = longestPhrase(finalArray);
+    console.log(finalPhrase);
+  }
 
 
