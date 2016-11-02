aeee2d027d4da30fefa464ff45fb46bae52c0256
jyoung488 <jyoung488@gmail.com>
2016-11-02 15:07:02 -0700

    Define add workouts method and user input

--- databases/Exercise-Log/Exercise_Log.rb
+++ databases/Exercise-Log/Exercise_Log.rb
@@ -4,74 +4,39 @@ require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("workouts.db")
-db.execute("PRAGMA foreign_keys = ON")
+db.results_as_hash = true
 
-create_workouts_table = <<-SQL
+create_workouts = <<-SQL
   CREATE TABLE IF NOT EXISTS workouts(
     id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    gym_id INT,
-    friend_id INT,
-    day REAL,
-    FOREIGN KEY(gym_id) REFERENCES gyms(id),
-    FOREIGN KEY(friend_id) REFERENCES friends(id)
+    exercise VARCHAR(255),
+    gym VARCHAR(255),
+    friend VARCHAR(255),
+    day REAL
   )
 SQL
 
-create_gyms = <<-SQL
-  CREATE TABLE IF NOT EXISTS gyms(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    city VARCHAR(255)
-  )
-SQL
+db.execute(create_workouts)
 
-create_friends = <<-SQL
-  CREATE TABLE IF NOT EXISTS friends(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-  )
-SQL
-
-db.execute(create_workouts_table)
-db.execute(create_gyms)
-db.execute(create_friends)
-
-def add_workout(db, workout, gym, friend)
-  db.execute("INSERT INTO workouts (name) VALUES (?)", [workout])
+def add_workout(db, exercise, gym, friend, day)
+   db.execute("INSERT INTO workouts(exercise, gym, friend, day) VALUES (?, ?, ?, ?)", [exercise, gym, friend, day])
 end
-
-def add_gym(db, gym)
-  db.execute("INSERT INTO gyms (name) VALUES (?)", [gym])
-end
-
-def add_friend(db, friend)
-  db.execute("INSERT INTO friends (name) VALUES (?)", [friend])
-end
-
-def past_workouts(db)
-   workouts = db.execute("SELECT workouts.day, gyms.name, workouts.name, friends.name
-    FROM workouts, gyms, friends
-    WHERE workouts.gym_id=gyms.id
-    AND workouts.friend_id=friends.id")
-   workouts
-end
-
-def view_friends(db)
-  friends = db.execute ("SELECT * FROM friends")
-  friends
-end
-
-def view_gyms(db)
-  gyms = db.execute("SELECT * FROM gyms")
-  gyms
-end
-
-# add_workout(db, 'yoga', 1, 1)
-past_workouts(db)
 
 # USER INTERFACE
 
+# puts "Hello! Welcome to your exercise log. Here are your past workouts:"
+# past_workouts(db)
 
+puts "What's the date in YYYY-MM-DD format?"
+day = gets.chomp
 
+puts "What workout did you do?"
+exercise = gets.chomp
 
+puts "What gym did you go to?"
+gym = gets.chomp
+
+puts "Who did you go with?"
+friend = gets.chomp
+
+add_workout(db, exercise, gym, friend, day)
