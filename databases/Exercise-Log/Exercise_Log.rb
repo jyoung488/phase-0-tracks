bb65d90502bbcb58c53aa50599c0c2ea7886f5f9
jyoung488 <jyoung488@gmail.com>
2016-11-03 10:20:06 -0700

    Define add and list methods for gyms and friends

--- databases/Exercise-Log/Exercise_Log.rb
+++ databases/Exercise-Log/Exercise_Log.rb
@@ -49,41 +49,63 @@ def add_workout(db, exercise, gym, friend, day)
    db.execute("INSERT INTO workouts(exercise, gym, friend, day) VALUES (?, ?, ?, ?)", [exercise, gym, friend, day])
 end
 
-# define method that shows all gyms
+def add_gym(db, gym_name, city)
+  db.execute("INSERT INTO gyms(name, city) VALUES (?, ?)", [gym_name, city])
+end
+
+def list_gyms(db)
+  all_gyms = db.execute("SELECT * FROM gyms")
+
+  all_gyms.each do |gym|
+    puts "#{gym['id']} - #{gym['name']}: #{gym['city']}"
+  end
+end
+
+def add_friend(db, friend_name)
+  db.execute("INSERT INTO friends(friend_name) VALUES (?)", [friend_name])
+end
+
+def list_friends(db)
+  all_friends = db.execute("SELECT * FROM friends")
+
+  all_friends.each do |friend|
+    puts "#{friend['id']} - #{friend['friend_name']}"
+  end
+end
 
 # define method that shows all friends
 
 # USER INTERFACE
 
-puts "Hello! Welcome to your exercise log. Here are your past workouts:"
-past_workouts(db)
+# puts "Hello! Welcome to your exercise log. Here are your past workouts:"
+# past_workouts(db)
 
-# ask user if they would like to view tables or add info
-  # if view
-    # ask if they want to view gyms, friends, or workouts
-      # if gyms - show all columns from gyms table
-      # if friends - show all columns from friends table
-      # if workouts - past_workouts(db)
-  # add info to gyms, friends, or workouts
-    # if workouts - add_workout
-    # if gyms - add_gym
-    # if friends - add_friend
+# # ask user if they would like to view tables or add info
+#   # if view
+#     # ask if they want to view gyms, friends, or workouts
+#       # if gyms - show all columns from gyms table
+#       # if friends - show all columns from friends table
+#       # if workouts - past_workouts(db)
+#   # add info to gyms, friends, or workouts
+#     # if workouts - add_workout
+#     # if gyms - add_gym
+#     # if friends - add_friend
 
-  # repeat until 'exit'
+#   # repeat until 'exit'
 
-puts "What's the date in YYYY-MM-DD format?"
-day = gets.chomp
+# puts "What's the date in YYYY-MM-DD format?"
+# day = gets.chomp
 
-puts "What workout did you do?"
-exercise = gets.chomp
+# puts "What workout did you do?"
+# exercise = gets.chomp
 
-puts "What gym did you go to?"
-gym = gets.chomp
+# puts "What gym did you go to?"
+# gym = gets.chomp
 
-puts "Who did you go with?"
-friend = gets.chomp
+# puts "Who did you go with?"
+# friend = gets.chomp
 
-add_workout(db, exercise, gym, friend, day)
+# add_workout(db, exercise, gym, friend, day)
 
-puts "Thank you! Here's your past workouts now:"
-past_workouts(db)
+# puts "Thank you! Here's your past workouts now:"
+# past_workouts(db)
