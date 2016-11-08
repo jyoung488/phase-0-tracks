2949409cb7155857001cff965a18f376af030189
jyoung488 <jyoung488@gmail.com>
2016-11-08 14:34:36 -0800

    Add style change to text

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,3 +6,6 @@ var image = document.getElementsByTagName("img");
 var firstImage = image[0];
 
 firstImage.addEventListener ('click', addBorder);
+
+var text = document.getElementsByTagName("p")[0];
+text.style.fontSize ="200px";
