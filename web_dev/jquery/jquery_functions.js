5cd6b29d292a671de2dd45b3cdecf6b609903931
jyoung488 <jyoung488@gmail.com>
2016-11-09 16:38:41 -0800

    Add CSS to page

--- web_dev/jquery/jquery_functions.js
+++ web_dev/jquery/jquery_functions.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
   $("button").click(function() {
     $("li").slideToggle(1000);
-  })
+  });
+
+  $("h1").addClass("header");
+
+  $("button").addClass("button");
 });
